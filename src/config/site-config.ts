6e5593b69192ebbb718854a7b5c48c4c5970ce5e
Kunal Garg <kunalgarg054@gmail.com>
2024-12-31 15:29:41 +0530

    rolled back, responsive footer, added the metadata

---
+++ src/config/site-config.ts
@@ -0,0 +1,62 @@
+import { Metadata } from "next";
+
+const TITLE = "Onchain-Aura";
+const DESCRIPTION = "Discover Your Aura Score based on your onchain journey";
+
+const BASE_URL = "https://onchainaura.fun";
+
+
+export const siteConfig: Metadata = {
+    title: TITLE,
+    description: DESCRIPTION,
+    icons: {
+        icon: "/favicon.ico",
+    },
+    applicationName: TITLE,
+    openGraph: {
+        type: "website",
+        locale: "en_IE",
+        url: BASE_URL,
+        title: TITLE,
+        description: DESCRIPTION,
+        images: [
+            {
+                url: "/favicon.ico",
+                width: 1200,
+                height: 630,
+                alt: TITLE,
+            },
+        ],
+    },
+    category: "Cryptocurrency",
+    alternates: {
+        canonical: BASE_URL,
+    },
+    twitter: {
+        creator: '@onchainaura_fun',
+        title: TITLE,
+        description: DESCRIPTION,
+        card: 'summary_large_image',
+        images: [
+            {
+                url: "/favicon.ico",
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
+    keywords: [
+        "onchain",
+        "aura",
+        "ethereum",
+        "nft",
+        "cryptocurrency",
+        "web3",
+        "blockchain",
+        "decentralized",
+        "decentralized finance",
+        "defi",
+        "defi score",
+    ],
+    metadataBase: new URL(BASE_URL),
+};
